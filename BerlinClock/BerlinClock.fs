5f23f8fd0d2c6ec4b85e9b453b5ee145525d9249
Max Malook <community@malook.de>
2014-05-28 19:17:50 +0200

    zero time implemented

---
+++ BerlinClock/BerlinClock.fs
@@ -0,0 +1,20 @@
+﻿module BerlinClock
+
+let clock _ = """Y
+OOOO
+OOOO
+OOOOOOOOOOO
+OOOO"""
+
+open Xunit
+open FsUnit.Xunit
+
+[<Fact>]
+let Zero ()=
+    "00:00:00"
+    |> clock
+    |> should equal """Y
+OOOO
+OOOO
+OOOOOOOOOOO
+OOOO"""
